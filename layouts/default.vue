3d1dd09a5b48953a9855b9726c26506c6dc6beaa
Sebastian Landwehr <landwehr@zebralog.de>
2020-04-15 13:33:45 +0200

    init

---
+++ layouts/default.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    <footer class="navbar">
+      <div class="navbar-item">Informationen</div>
+      <div class="navbar-item">Karte</div>
+      <div class="navbar-item">Diskussion</div>
+    </footer>
+  </div>
+</template>
---
+++ nuxt.config.js
@@ -0,0 +1,12 @@
+export default {
+  head: {
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: 'Interpart mobiler Prototyp' },
+    ],
+  },
+  css: [
+    '~/assets/style.scss',
+  ],
+}
