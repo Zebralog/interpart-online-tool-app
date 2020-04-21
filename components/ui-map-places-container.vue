e9429e9c7d61cefdd0adbdf2bb2513c269ee8839
giuseppe roccazzella <roccazzella@zebralog.de>
2020-04-21 12:28:32 +0200

    small fix on map places container style;

--- components/ui-map-places-container.vue
+++ components/ui-map-places-container.vue
@@ -23,7 +23,7 @@ export default {
     elementStyle: () => {
       return {
         width: "100%",
-        height: "100%",
+        height: "90%",
         position: `fixed`,
         top: `0`,
         left: `0`,
